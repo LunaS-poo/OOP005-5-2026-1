e314f00b2fc8b8a05756c4f33bd3e83dbc71f020
LunaS-poo <lscastellanosg@udistrital.edu.co>
2026-02-15 22:44:09 -0500

    Add files via upload

---
+++ Corte1/Practica1/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * Feb 2/2026
+ * Luna Castellanos
+ */
+package helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+        // TODO code application logic here
+    }
+    
+}

dfa8bbfd1b91a56d541ae5132e03005ecafc4c2a
LunaS-poo <lscastellanosg@udistrital.edu.co>
2026-03-26 09:13:16 -0500

    Matriz 2x2 con suma y multiplicaciòn

--- Corte1/Practica4/Matriz2x2.java
+++ Corte1/Practica4/Matriz2x2.java
@@ -37,9 +37,55 @@ public class Matriz2x2 {
     }
     
     public static void main(String[] args) {
-        Matriz2x2 m = new Matriz2x2();
-        m.set();
-        m.get();
-        
-    }   
+    Matriz2x2 m = new Matriz2x2();
+    Matriz2x2 n = new Matriz2x2();
+
+    System.out.println("Ingrese la primera matriz:");
+    m.set();
+
+    System.out.println("Ingrese la segunda matriz:");
+    n.set();
+
+    Matriz2x2 resultado = m.sumar(n);
+    Matriz2x2 resultadoM = m.multiplicar(n);
+    System.out.println("\nPrimera matriz:");
+    m.get();
+
+    System.out.println("\nSegunda matriz:");
+    n.get();
+
+    System.out.println("\nSuma de matrices:");
+    resultado.get();
+    
+    System.out.println("\nMultiplicacion de matrices:");
+    resultadoM.get();
 }
+     public Matriz2x2 sumar(Matriz2x2 otra) {
+
+     Matriz2x2 resultado = new Matriz2x2();
+     for (int i = 0; i < filas; i++) {
+     for (int j = 0; j < columnas; j++) {
+     resultado.data[i][j] =
+
+    this.data[i][j] + otra.data[i][j];
+}
+}
+return resultado;
+ }
+    public Matriz2x2 multiplicar(Matriz2x2 otra) {
+
+    Matriz2x2 resultadoM = new Matriz2x2();
+
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            resultadoM.data[i][j] = 0;
+
+            for (int k = 0; k < columnas; k++) {
+                resultadoM.data[i][j] += this.data[i][k] * otra.data[k][j];
+            }
+        }
+    }
+
+    return resultadoM;
+} 
+ }
